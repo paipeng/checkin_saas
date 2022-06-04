17180d24dfa8d930814a7c8177b9c8a50744c131
Pai Peng <sipaipv6@gmail.com>
2022-06-05 00:01:19 +0800

    add location parameters

--- src/main/java/com/paipeng/saas/checkin/tenant/entity/Record.java
+++ src/main/java/com/paipeng/saas/checkin/tenant/entity/Record.java
@@ -18,6 +18,25 @@ public class Record extends BaseEntity{
     @Column(name = "latitude", precision = 11, scale = 8)
     private BigDecimal latitude;
 
+    @Column(name = "ip", length = 15)
+    private String ip;
+
+    @Column(name = "country", length = 45)
+    private String country;
+
+    @Column(name = "province", length = 50)
+    private String province;
+
+    @Column(name = "city", length = 50)
+    private String city;
+
+    @Column(name = "district", length = 50)
+    private String district;
+
+    @Column(name = "address", length = 100)
+    private String address;
+
+
     @ManyToOne(cascade = {CascadeType.REFRESH}, fetch = FetchType.LAZY)
     @JoinColumn(name = "code_id", referencedColumnName = "id", nullable = false)
     @LazyToOne(value = LazyToOneOption.FALSE)
@@ -72,4 +91,56 @@ public class Record extends BaseEntity{
     public void setDevice(Device device) {
         this.device = device;
     }
+
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
 }
